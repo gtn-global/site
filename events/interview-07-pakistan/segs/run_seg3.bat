31cd783fd4731ef4d3fb228f1377e0895f992635
Bruce-Creator <brucedong2008@gmail.com>
2026-08-17 21:54:24 +0800

    chore: 移除 INTERVIEW-07 转写素材(音频/segs)，仅保留正文页

--- events/interview-07-pakistan/segs/run_seg3.bat
+++
@@ -1,1 +0,0 @@
-cmd /c whisper C:\Users\bruce\Desktop\bruce\GTN-site\gtn-global-site\gtn-global-site-3.0\events\interview-07-pakistan\segs\seg_3.wav --model small --language zh --beam_size 1 --output_format txt --output_dir C:\Users\bruce\Desktop\bruce\GTN-site\gtn-global-site\gtn-global-site-3.0\events\interview-07-pakistan\segs --verbose False
